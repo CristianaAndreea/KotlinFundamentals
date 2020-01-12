8c03b12ae8e43efe5cfa2c838ee7ce142856f374
Andreea Cristiana <andreea.braesteanu@gmail.com>
2020-01-12 20:12:15 +0200

    completed color my views tutorial

--- app/src/main/java/com/tutorial/kotlinfundamentals/ColorMyViewsActivity.kt
+++ app/src/main/java/com/tutorial/kotlinfundamentals/ColorMyViewsActivity.kt
@@ -3,6 +3,7 @@ package com.tutorial.kotlinfundamentals
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_color_my_views.*
 
@@ -16,10 +17,15 @@ class ColorMyViewsActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+
         val clickableViews: List<View> =
             listOf(
                 box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, constraint_layout
+                box_four_text, box_five_text, constraint_layout,
+                redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
@@ -35,6 +41,9 @@ class ColorMyViewsActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
